f1bd0cd3a566fac342a4854ea9ac2b3210ec3a32
jurassicplayer <jurassicplayer75@gmail.com>
2022-05-09 00:48:20 -0700

    Fix: Preserve author folders in dat

--- dist/remove_unneeded_files.sh
+++ dist/remove_unneeded_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sed -i '/cover.jpg/d' 'OTLN.dat'
-sed '/<machine name="Calibre Library">/{:a;N;/<\/machine>/!ba};/<description>Calibre Library/d' 'OTLN.dat' > 'OTLN_metadata.dat'
+sed '/<machine name="Default">/{:a;N;/<\/machine>/!ba};/<description>Default/d' 'OTLN.dat' > 'OTLN_metadata.dat'
 rm 'OTLN.dat'

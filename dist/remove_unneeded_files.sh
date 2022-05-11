2f9558180eadd02cbe938a5dee4608a28195e2e6
jurassicplayer <jurassicplayer75@gmail.com>
2022-05-11 00:30:34 -0700

    Revert: Determined better solution for covers

--- dist/remove_unneeded_files.sh
+++ dist/remove_unneeded_files.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-#sed -i '/cover.jpg/d' 'OTLN.dat'
+sed -i '/cover.jpg/d' 'OTLN.dat'
 sed '/<machine name="Default">/{:a;N;/<\/machine>/!ba};/<description>Default/d' 'OTLN.dat' > 'OTLN_metadata.dat'
 rm 'OTLN.dat'
-
-# Copy covers from my library to a temporary location
-rsync -avm --include='*.jpg' -f 'hide,! */' "../Calibre Library/" "./covers"
-# Archive the covers
-7z a covers.7z ./covers/*

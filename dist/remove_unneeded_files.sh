97231abf70245ae4eca7b05dd27d38b22cde808a
jurassicplayer <jurassicplayer75@gmail.com>
2022-05-10 20:08:52 -0700

    Fix: Update dat and include covers in dat

--- dist/remove_unneeded_files.sh
+++ dist/remove_unneeded_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sed -i '/cover.jpg/d' 'OTLN.dat'
+#sed -i '/cover.jpg/d' 'OTLN.dat'
 sed '/<machine name="Default">/{:a;N;/<\/machine>/!ba};/<description>Default/d' 'OTLN.dat' > 'OTLN_metadata.dat'
 rm 'OTLN.dat'

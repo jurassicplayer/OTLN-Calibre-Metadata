d9f8aaff079659f49271b7182ae814f15eee8490
jurassicplayer <jurassicplayer75@gmail.com>
2022-05-12 18:04:05 -0700

    Fix: Pre-dat script to update local calibredb

--- dist/pre-dat-processing.sh
+++ dist/pre-dat-processing.sh
@@ -3,12 +3,15 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 
 # Replace <br><br> with </p><p>
+echo "Replace '<br>'"
 egrep -lir --include="*.opf" "&lt;br&gt;&lt;br&gt;" "$SCRIPT_DIR/../metadata" | xargs -d '\n' sed -i 's/\&lt;br\&gt;\&lt;br\&gt;/\&lt;\/p\&gt;\&lt;p\&gt;/g'
 # Replace <br> with </p><p>
 egrep -lir --include="*.opf" "&lt;br&gt;" "$SCRIPT_DIR/../metadata" | xargs -d '\n' sed -i 's/\&lt;br\&gt;/\&lt;\/p\&gt;\&lt;p\&gt;/g'
 # Remove extra space before closing </p> tags
+echo "Remove ' </p>'"
 egrep -lir --include="*.opf" " &lt;/p&gt;" "$SCRIPT_DIR/../metadata" | xargs -d '\n' sed -i 's/ \&lt;\/p\&gt;/\&lt;\/p\&gt;/g'
 # Remove id="book-description-full" from <p id="book-description-full">
+echo "Remove 'id=\"book-description-full\"'"
 egrep -lir --include="*.opf" " id=\"book-description-full\"" "$SCRIPT_DIR/../metadata" | xargs -d '\n' sed -i 's/ id=\"book-description-full\"//g'
 
 
@@ -18,3 +21,6 @@ egrep -lir --include="*.opf" "&lt;p&gt;&lt;strong&gt;.*?&lt;/strong&gt;&lt;/p&gt
 # Check for explicit color changes to remove them manually
 echo "-[ Remove text color overrides ]-"
 egrep -lir --include="*.opf" "style=\"color:" "$SCRIPT_DIR/../metadata"
+
+
+calibredb restore_database --really-do-it
